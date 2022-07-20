a4d8391e1d4686ca549a3c5c2d8f44328d2bce10
YeonJun Cho <micayel1219@gmail.com>
2022-07-20 14:14:20 +0900

    tomcat setting

---
+++ src/main/java/com.example.HelloServlet/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.example.HelloServlet;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 들어오는 파라미터를 UTF-8로 해석
+        req.setCharacterEncoding("UTF-8");
+        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
+        resp.setCharacterEncoding("UTF-8");
+
+        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+        resp.setContentType("text/html; charset=utf-8");
+
+        resp.getWriter().append("안녕하세요!");
+    }
+}
