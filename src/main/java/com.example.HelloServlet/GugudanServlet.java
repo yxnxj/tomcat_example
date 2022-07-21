4117ffd544642c47251adf1617febc8a0157c7fd
YeonJun Cho <micayel1219@gmail.com>
2022-07-21 13:07:18 +0900

    구구단 커밋

--- src/main/java/com.example.HelloServlet/GugudanServlet.java
+++ src/main/java/com.example.HelloServlet/GugudanServlet.java
@@ -9,19 +9,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 들어오는 파라미터를 UTF-8로 해석
         req.setCharacterEncoding("UTF-8");
-        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
         resp.setCharacterEncoding("UTF-8");
-
-        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
         resp.setContentType("text/html; charset=utf-8");
 
-        resp.getWriter().append("안녕하세요!");
-    }
+        int dan = Integer.parseInt(req.getParameter("dan"));
+        int limit = Integer.parseInt(req.getParameter("limit"));
 
+        resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+        }
+    }
 }
